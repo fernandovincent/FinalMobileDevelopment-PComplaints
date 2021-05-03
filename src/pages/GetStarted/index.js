2688c357728c1638beefffad13fc91fac711d128
GraceFeibe21 <s11810232@student.unklab.ac.id>
2021-05-03 14:04:08 +0800

    commit SignIn

--- src/pages/GetStarted/index.js
+++ src/pages/GetStarted/index.js
@@ -1,32 +1,39 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import Button from '../../components/atoms/button'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Button from '../../components/atoms/Button';
+import Gap from '../../components/atoms/Gap';
 
-const GetStarted = () => {
+const GetStarted = ({navigation}) => {
   return (
     <View style={styles.page}>
-      <Text style={styles.text}>Giving complaints is easier and more flexible</Text>
-      <Gap height={175} />
-      <Button  title="Get Started" color="#112340" textColor='white'>Get Started</Button>
-      <Button  title="Sign In" color="#7D8797" textColor='white'>Sign In</Button>
+      <View style={styles.contentWrapper}>
+      <Gap height={50} />
+        <Text style={styles.text}>Giving complaints is easier and more flexible</Text>
+        <Gap height={200} />
+        <Button title="Get Started" color="#112340" textColor='white' onPress={() => navigation.navigate('SignUp')} />
+        <Gap height={17} />
+        <Button title="Sign In" color="#7D8797" textColor='white' onPress={() => navigation.navigate('SignIn')} />
+      </View>
     </View>
-  )
-}
+  );
+};
 
 export default GetStarted;
 
 const styles = StyleSheet.create({
   page: {
+    padding: 24,
     backgroundColor: '#92E3A9',
     flex: 1,
-    //alignItems: 'center',
-    //ustifyContent: 'center',
   },
   text:{
-    marginTop:50,
-    marginLeft:25,
     color:'#112340',
     fontSize: 28,
     fontFamily:'Montserrat-SemiBold',
+  },
+  contentWrapper: {
+      paddingHorizontal: 24,
+      paddingTop: 26,
+      marginTop: 24,
   },
 });
