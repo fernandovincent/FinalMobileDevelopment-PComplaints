8f17ffd2d6b6a8e232136b9f5c621b24ec52f11f
Thurion <s21810263@student.unklab.ac.id>
2021-05-10 01:40:00 +0800

    commit home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,49 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, {useState, useEffect} from 'react';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import Card from '../../components/molecules/Card';
+import firebase from '../../config/Firebase';
+import Header2 from '../../components/molecules/Header2';
 
 const Home = () => {
+
+  const [dataLaporan, setDataLaporan] = useState([]);
+
+  useEffect(() => {
+    const uid = firebase.auth().currentUser.uid;
+    firebase.database().ref(`reports/${uid}`).on("value", (res) => {
+      if(res.val()){
+        //Ubah menjadi array
+        //Object.keys mengubah data menjadi array
+        const rawData = res.val();
+        const usersArr = [];
+        Object.keys(rawData).map((item) => {
+          usersArr.push({
+            id: item,
+            ...rawData[item]
+          });
+        });
+        setDataLaporan(usersArr);
+      }
+    });
+  }, []);
+
+  console.log(dataLaporan);
+
   return (
     <View>
-      <Text>Home Page</Text>
+    <ScrollView>
+    <Header2 title="Homepage" />
+    <View>
+      {dataLaporan.map(item => (
+            <Card
+              key={item.id}
+              name={item.name}
+              tanggalKejadian={item.tanggalKejadian}
+              isiLaporan={item.isiLaporan}
+            />
+          ))}
+    </View>
+    </ScrollView>
     </View>
   )
 }
