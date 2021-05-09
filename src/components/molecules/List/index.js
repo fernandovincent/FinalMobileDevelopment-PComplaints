91f85a3c54ed815c60d7c99812b4ed3f29cb3677
JLuntungan <s11810192@student.unklab.ac.id>
2021-05-09 22:43:00 +0800

    commit List

---
+++ src/components/molecules/List/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'; 
+import { IconEditProfile, IconLogout, IconNext } from '../../../assets';
+
+const List = ({name, desc, onPress, icon}) => {
+  const Icon = () => {
+    if (icon === 'edit-profile'){
+      return <IconEditProfile />
+    }
+    if (icon === 'logout'){
+      return <IconLogout />
+    }
+  };
+
+  return (
+    <TouchableOpacity style={styles.container} onPress={onPress}>
+      <View>
+      <Icon />
+      </View>
+      <View style={styles.content}>
+        <Text style={styles.name}>{name}</Text>
+        <Text style={styles.desc}>{desc}</Text>
+      </View>
+      <View style={styles.icon}>
+      <IconNext /> 
+      </View>
+    </TouchableOpacity>
+  )
+} 
+
+export default List;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    padding: 16,
+    alignItems: 'center',
+  },
+  content: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#EEEEEE",
+    marginLeft: 15,
+  },
+  name: {
+    fontSize: 16,
+    fontFamily: "Montserrat-Regular",
+    color: "#112340",
+    marginRight: 200,
+  },
+  desc: {
+    fontSize: 12,
+    fontFamily: "Montserrat-Light",
+    color: "#7D8797",
+  },
+  icon: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+});
--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,5 +1,6 @@
 import Header from './Header';
 import Loading from './Loading';
 import BottomNavigator from './BottomNavigator';
+import List from './List';
 
-export {Header, Loading, BottomNavigator};
+export {Header, Loading, BottomNavigator,List};
